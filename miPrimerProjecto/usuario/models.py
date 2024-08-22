70f54bf2ba1064b405ee095a32094aa3fb3fda98
advg <advguwu.09.02.2005@gmail.com>
2024-08-21 21:28:34 -0500

    Creamos el modelo de BD de los usuarios

--- miPrimerProjecto/usuario/models.py
+++ miPrimerProjecto/usuario/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    ESTADO_CHOICES = [
+        ('activo','Activo'),
+        ('inactivo','Inactivo')
+    ]
+    nombre = models.CharField(max_length=50)
+    cedula = models.CharField(max_length=11)
+    telefono = models.CharField(max_length=15)
+    email = models.EmailField(max_length=100)
+    estado=models.CharField(max_length=50,choices=ESTADO_CHOICES,default='activo')
+    fechaCreate =models.DateField(auto_now=True) 
+    
+    def __str__(self):
+        return self.nombre
