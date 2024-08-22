b8921a557beefce3f34ab7834f7b3f2be5564e73
advg <advguwu.09.02.2005@gmail.com>
2024-08-21 22:11:07 -0500

    Creo las vistas que nos permitiran crear y listar un nuevo usuario de acuerdo al modelo

--- miPrimerProjecto/usuario/views.py
+++ miPrimerProjecto/usuario/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Persona
+from .form import PersonaForm
 
 # Create your views here.
+def listarUsuarios(request):
+    personas = Persona.objects.all()
+    return render(request,"listarUsuarios.html",{'personas':personas})
+def registrarUsuario(request):
+    if request.method == 'POST':
+        form = PersonaForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect(listarUsuarios)
+    
+    else:
+        form = PersonaForm()
+        return render(request,'registrarUsuario.html',{'form':form})
